8dfd86030da3a6faf8b7580958c537c7a37819a9
Jim Phillips <jamminjimi@gmail.com>
2023-03-02 14:03:45 -0500

    Add project files.

---
+++ Models/SalesInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BlazorAppDevxTest.Models
+{
+    public class SaleInfo
+    {
+        public int OrderId { get; set; }
+        public string Region { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ Services/ISalesInfoDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using BlazorAppDevxTest.Models;
+
+namespace BlazorAppDevxTest.Services
+{
+    public interface ISalesInfoDataProvider
+    {
+        Task<IEnumerable<SaleInfo>> GetSalesAsync(CancellationToken ct = default);
+        Task<IEnumerable<SaleInfo>> GetReducedSalesAsync(CancellationToken ct = default);
+    }
+}
